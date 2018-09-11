dae653b9b4962e05dabb971e3140b1ef02269d8d
Lucas Brito de Morais <42049148+lucasbrito2000@users.noreply.github.com>
2018-09-11 10:11:15 -0300

    Create verificação_jogovelha.py
    #14

---
+++ src/verificação_jogovelha.py
@@ -0,0 +1,63 @@
+SE JOGADOR da vez JOGAR NA posição 1 E posição não estiver marcada FAÇA
+	
+	marcar posição 
+SENAO
+	mensagem:erro posição ocupada	
+	
+
+SE JOGADOR da vez JOGAR NA posição 1 E posição não estiver marcada FAÇA
+	
+	marcar posição  
+SENAO
+	mensagem:erro posição ocupada	
+	
+
+SE JOGADOR da vez JOGAR NA posição 1 E posição não estiver marcada FAÇA
+	
+	marcar posição  
+SENAO
+	mensagem:erro posição ocupada	
+	
+
+SE JOGADOR da vez JOGAR NA posição 1 E posição não estiver marcada FAÇA
+	
+	marcar posição  
+SENAO
+	mensagem:erro posição ocupada	
+	
+
+SE JOGADOR da vez JOGAR NA posição 1 E posição não estiver marcada FAÇA
+	
+	marcar posição  
+SENAO
+	mensagem:erro posição ocupada	
+	
+
+
+SE JOGADOR da vez JOGAR NA posição 1 E posição não estiver marcada FAÇA
+	
+	marcar posição  
+SENAO
+	mensagem:erro posição ocupada	
+
+SE JOGADOR da vez JOGAR NA posição 1 E posição não estiver marcada FAÇA
+	
+	marcar posição  
+SENAO
+	mensagem:posição ocupada	
+
+SE JOGADOR da vez JOGAR NA posição 1 E posição não estiver marcada FAÇA
+	
+	marcar posição  
+SENAO
+	mensagem:erro posição ocupada	
+
+SE JOGADOR da vez JOGAR NA posição 1 E posição não estiver marcada FAÇA
+	
+	marcar posição  
+SENAO
+	mensagem:erro posição ocupada	
+SE jogador da vez da ocupar 3 posições em linhda reta 
+	mensagem:ganhou
+senao todas as casa ocupadas 
+	mensagem:velha
